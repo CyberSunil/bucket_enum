49bc8dd4b295dd7aa74fb7268862225ae782a917
Sunil Tripathy <143227935+CyberSunil@users.noreply.github.com>
2023-08-26 04:07:09 +0530

    Create s3_bucket_enumeration.py
    Commit

---
+++ s3_bucket_enumeration.py
@@ -0,0 +1,38 @@
+import boto3
+
+def enumerate_s3_buckets(target_domain):
+  """
+  Enumerates all S3 buckets for a given domain.
+
+  Args:
+    target_domain: The domain name to enumerate.
+
+  Returns:
+    A list of all S3 buckets for the given domain.
+  """
+
+  s3 = boto3.client('s3')
+
+  buckets = []
+
+  for bucket_name in s3.list_buckets()['Buckets']:
+    if target_domain in bucket_name['Name']:
+      buckets.append(bucket_name['Name'])
+
+  return buckets
+
+def main():
+  """
+  The main function.
+  """
+
+  target_domain = input('Enter the domain name to enumerate: ')
+
+  buckets = enumerate_s3_buckets(target_domain)
+
+  print('The following S3 buckets [🪣] found for the domain: {}'.format(target_domain))
+  for bucket in buckets:
+    print(bucket)
+
+if __name__ == '__main__':
+  main()
